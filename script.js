4bbe4bb71938ea32ff2c8a76a4962015ac986f8f
globalstyle <globaldev63@gmail.com>
2025-12-08 15:06:53 +0600

    Add event listeners to block context menu and keys
    Prevent default context menu and disable certain key combinations.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+ document.addEventListener('contextmenu', event => event.preventDefault());
 
+  document.onkeydown = function(e) {
+    // F12
+    if (e.keyCode == 123) {
+      return false;
+    }
+    // Ctrl+Shift+I/J/C
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74 || e.keyCode == 67)) {
+      return false;
+    }
+    // Ctrl+U
+    if (e.ctrlKey && e.keyCode == 85) {
+      return false;
+    }
+  }
