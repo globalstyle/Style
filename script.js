ddf023e88a9e5c293a54e30c2a51ce744abfc781
globalstyle <globaldev63@gmail.com>
2026-08-06 11:55:39 +0600

    update

--- script.js
+++ script.js
@@ -12,7 +12,8 @@
   var redirectRules = [
     {
       path: "/pages/all-made-in-italy",
-      redirectTo: "https://www.bored.com"
+      redirectTo: "https://www.shutterstock.com/image-photo/real-funny-face-captured-high-600w-62442307.jpg"
+
     }
   ];
 
